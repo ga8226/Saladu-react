25c58cd991db8a0ecd2bda2247ef84e816011c2f
ga8226 <ga8226@naver.com>
2023-07-26 17:44:00 +0900

    메뉴 맵 먼저 돌려보고 디비 빼기전 수정 메뉴 밑 언더 css 수정

--- src/layout/Content.js
+++ src/layout/Content.js
@@ -7,8 +7,8 @@ import Setup from '../constituent/Setup'
 export default function Content(props) {  
     return (
         <section  id={props.cid}>
-            <Banner id="mainBanner" ></Banner>
-            <Menu useid={props.usedb.navi[1].href}></Menu>
+            <Banner id="mainBanner"></Banner>
+            <Menu useid={props.usedb.navi[1].href} ></Menu>
             <Use useid={props.usedb.navi[0].href}></Use>
             <Form useid={props.usedb.navi[2].href}></Form>
             <Setup useid={props.usedb.navi[3].href}></Setup>
