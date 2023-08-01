47f9c9e398b4368a7e0e239f937040df59ec93af
ga8226 <ga8226@naver.com>
2023-08-01 13:00:46 +0900

    폰트 추가하고 푸터 폰트 수정 푸터 반응형 잡기

--- src/layout/Ft.js
+++ src/layout/Ft.js
@@ -2,21 +2,34 @@ export default function Ft(props) {
     return (
         <>
             <footer id="ft">
-                <div className="container pt-3 pt-lg-5 px-0 px-lg-3 mx-0 mx-lg-auto">
-                    <div>
-                         <img src="../img/logo.svg" alt=""  className=""/>
+                <div className="container pt-lg-4 px-lg-3  mx-lg-auto">
+                    <div className="d-flex justify-content-between align-items-center">
+                         <img src="../img/logo.svg" alt="푸터로고" className="img-fluid"/>
+                         <div>
+                            <ul className="d-lg-flex mb-0 d-md-none">
+                                <li><a href="#none">고객문의</a></li>
+                                <li className="px-3"><a href="#none">창업문의</a></li>
+                            </ul>
                     </div>
-                    <div>
-                        <ul className="d-flex">
-                            <li className="px-3"><span>(주) 샐러드유</span>대전광역시 유성구 문지로 291,206호</li>
-                            <li className="px-3"><span>사업자 등록번호</span>831-81-02134</li>
-                            <li className="px-3"><span>대표</span>유미정</li>
-                        </ul>
-                        <ul className="d-flex">
-                            <li className="px-3"><span>Tel</span>1544-7386</li>
-                            <li className="px-3"><span>E-mail</span>epfree@naver.com</li>
-                        </ul>
                     </div>
+                    <div className="pt-4">
+                        <div>
+                            <ul className="d-flex">
+                                <li><span>(주) 샐러드유</span>대전광역시 유성구 문지로 291,206호</li>
+                                <li className="px-3"><span>사업자 등록번호</span>831-81-02134</li>
+                                <li className="px-3"><span>대표</span>유미정</li>                                
+                            </ul>
+                            <ul className="d-md-flex d-lg-none">
+                                <li><a href="#none">고객문의</a></li>
+                                <li className="px-3"><a href="#none">창업문의</a></li>
+                            </ul>
+                            <ul className="d-flex">
+                                <li><span>Tel</span>1544-7386</li>
+                                <li className="px-3"><span>E-mail</span>epfree@naver.com</li>
+                            </ul>
+                        </div>
+                    </div>
+                    
                 </div>
             </footer>
         
