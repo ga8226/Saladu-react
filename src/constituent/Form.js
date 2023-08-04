7938b78d25b0a447a6dd268780e56701a4b60554
ga8226 <ga8226@naver.com>
2023-08-04 18:12:31 +0900

    폼태그 쪽 구성하고 css 수정

--- src/constituent/Form.js
+++ src/constituent/Form.js
@@ -1,11 +1,31 @@
+import db from '../data/db.json'
+import React,{ useState } from 'react'
 export default function Form(props) {  
+ const [openp, openSet] = useState(false); 
+
     return (
         <>
-            <form id={props.useid} style={{height : "100vh"}}>
-                <div>
+            <form id={props.useid} className='py-5'>
+                <div className="py-5 container my-5">
+                    
+                        <h2 className="center">{db.Form.Ftitle}</h2>
+                   
+                    <ul className='col-md-10 mx-auto'>
+                        {
+                            db.Form.Qlist.map((v,i)=>{
+                                return(
+                                    <li className='center'>
+                                        <h3 id='Qbox' onClick={()=>{ openSet(!openp);  }}><div className='number'></div>{v.Q}</h3>
+                                        { openp && <p>{v.A}</p> }
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
                 
                 </div> 
             </form>
         </>
     )
 }
+
