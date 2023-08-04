20b09a866ff57658950987e88f7453778f6fe11b
ga8226 <ga8226@naver.com>
2023-08-04 14:05:19 +0900

    이용방법 버튼 부트스트랩 아이콘 추가하고 css 수정

--- src/constituent/Form.js
+++ src/constituent/Form.js
@@ -3,7 +3,7 @@ export default function Form(props) {
         <>
             <form id={props.useid} style={{height : "100vh"}}>
                 <div>
-                폼태그테스트 만들곳 
+                
                 </div> 
             </form>
         </>
