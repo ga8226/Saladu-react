0092b5ba65acff7fa5d04f1410bc7716c26e28fc
ga8226 <ga8226@naver.com>
2023-09-15 17:17:51 +0900

    폼 직접입력 누르면 가라지게 하고 완성 new form 끝

--- src/constituent/Form.js
+++ src/constituent/Form.js
@@ -2,9 +2,13 @@ import React, { useState } from 'react';
 
 export default function Form(props) {
     const [showEmailInput, setShowEmailInput] = useState(false);
+    const [hideselect , sethideselect] = useState(true);
 
     const handleSelectChange = (e) => {
         const selectedValue = e.target.value;
+        if(selectedValue === 'show'){
+            sethideselect(false)
+        }
         setShowEmailInput(selectedValue === 'show');
     };
 
@@ -14,32 +18,33 @@ export default function Form(props) {
                 <div className="pt-3 pt-lg-5 position-relative">
                     <div className="container">
                         <div className='mb-3'>
-                            <h3 className="text-center pt-2">샐러드유 알림받고 선물 받자! 🎁</h3>
+                            <h3 className="pt-2">샐러드유 알림받고 선물 받자! 🎁</h3>
                         </div>
                         <div>
-                            <h5 className="text-center">이메일로 알림받기 🔔</h5>
-                            <div className={`d-flex justify-content-center pt-2 ${showEmailInput ? 'act' : ''}`} id="emailsection">
+                            <h5>이메일로 알림받기 🔔</h5>
+                            <div className={` pt-2 ${showEmailInput ? 'act' : ''}`} id="emailsection">
                                 <input type="text" name="email"></input><span>@</span>
-                                <select name="email" className="email-select" onChange={handleSelectChange}>
+                                { hideselect &&  <select name="email" className="email-select" onChange={handleSelectChange}>
                                     <option value="naver.com" selected>naver.com</option>
                                     <option value="google.com">google.com</option>
                                     <option value="daum.net">daum.net</option>
                                     <option value="show">직접입력</option>
-                                </select>
+                                </select> }
+                               
                                 {showEmailInput && (
                                     <input className="show" type="text" name="email" placeholder="직접입력"></input>
                                 )}
                             </div>
-                            <div>
+                            <div className=' kakao pt-4'>
                                 <a href="http://pf.kakao.com/_xjflLxj/chat" target="_blank">
-                                <h5 className="text-center">카카오톡으로 알림받기 📣</h5>
+                                <h5 >카카오톡으로 알림받기 📣</h5>
                                 </a>
-                                <div className="submit text-center">
+                                <div className="submit">
                                     <div className='mb-3'>
                                          <input type="checkbox" id="agree"></input>
                                          <label for="agree">개인정보정책동의</label>
                                     </div>
-                                    <input type="submit" value="제출하기" className="text-center"></input>
+                                    <input type="submit" value="제출하기"></input>
                         </div>
                             </div>
                         </div>
@@ -52,4 +57,3 @@ export default function Form(props) {
         </>
     )
 }
-
